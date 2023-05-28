86efc5fc3b7a57d756928e5a579d7f1159e97121
joel <joel.victor@databeat.io>
2023-05-28 18:25:16 +0530

    Add project files.

---
+++ Aid_For_Insomnia/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Aid_For_Insomnia.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
